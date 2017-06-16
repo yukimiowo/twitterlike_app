20dc0698a93a06ef565d9dc8f8226afd196a1860
Yukiko Kinoshita <yukiko-kinoshita@dcl.cs.waseda.ac.jp>
2017-06-16 10:02:28 +0000

    Edit layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #ページごとに完全なタイトルを返す
+  def full_title(page_title = "")
+    base_title = "TwitterlikeApp"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
