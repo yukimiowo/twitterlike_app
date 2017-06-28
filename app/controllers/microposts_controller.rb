85f16088243f4bf33b89a2855625467a2b2e220e
Yukiko Kinoshita <yukiko-kinoshita@dcl.cs.waseda.ac.jp>
2017-06-28 07:56:33 +0000

    correct some file

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user, only: :destroy
+  before_action :correct_micropost, only: :destroy
   
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -25,7 +25,7 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:content, :picture)
     end
     
-    def correct_user
+    def correct_micropost
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,10 +4,10 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-     respond_to do |format|
-       format.html { redirect_to @user }
-       format.js
-     end
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
   
   def destroy
